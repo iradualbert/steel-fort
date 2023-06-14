b0e109989fd4b24e29663727a993089600734727
abdulpro2000 <126849996+abdulpro2000@users.noreply.github.com>
2023-06-14 15:50:48 +0300

    added composite shapes functionality

--- BitirmeCalismasi.f
+++ BitirmeCalismasi.f
@@ -1,13 +1,12 @@
         program App_Steel
-        real :: pi
-        real :: d, tw, r, h, b, tf
+        real :: xcc,ycc,ixx,iyy
+        real :: d, tw, r, h, b, tf , A1 , A2 , Ix1, Ix2, Iy1, Iy2, cy2
         real :: X, Z, Area, Rgx, Rgy, SMx, SMz, Cx, Cy, Pmx,Pmy !properties to be calculated
-
         logical :: isFound
         character(len=6)  c,l
-        character(len=10) input
+        character(len=10) input, nameH, nameU,Name1,Name2
         character(len=10) name
-        character profileType
+        character(len=10) profileType
         character startAgain
         integer choice
         input = ""
@@ -43,19 +42,38 @@
 100     write(*,6)
 6       format(////,16x, "Please insert the name of the steel profile.")
         write(*,7)
-7       format(/,16x, "Or type INSERT to enter measurements")
+7       format(/,16x, "Or type INPUT/COMPOUND to enter measurements")
         read(*,*)input
 
         if(input .eq. "INPUT" .or. input .eq. "input") then
            call get_measurement_from_the_user
+        elseif(input .eq. "COMPOUND" .or. input .eq. "compound") then
+        open(92, file="Steel Sections Area-Inertia.csv", status="old")
+        read(92, *)  ! read and skip the header
+        write(*,91)
+91      format(/,16x, "Please choose the first compound section")
+        read(*,*)nameH
+        write(*,82)
+82      format(/,16x, "Please choose the second compound section")
+        read(*,*)nameU
+         do i=1,7
+           read(92, *)Name1,A1,Ixx1,Iyy1,b,d,Name2,A2,Ixx2,Iyy2,cy
+c          profileType1 = Name1(1:1)
+c          profileType2 = Name2(1:1)
+           if(trim(nameH) .eq. trim(Name1)) then
+           isFound = .true.
+           exit ! exit the loop if the the profile is found
+           end if
+        end do
+        close(92) ! always close the file
         else
         open(10, file="Steel Sections.csv", status="old")
         read(10, *)  ! read and skip the header
         isFound = .false.
         do i=1,14
-           read(10, *)name,d,b,tw,tf,r,h
+           read(10, *)Name,d,b,tw,tf,r,h
            profileType = name(1:1)
-           if(trim(name) .eq. trim(input)) then
+           if(trim(Name) .eq. trim(input)) then
            isFound = .true.
            write(*,*)name, d,b,tw,tf,r,h
            exit ! exit the loop if the the profile is found
@@ -86,21 +104,29 @@
         Area = calculate_Ar2(d,tw,r,h,b,tf)  !Area
         Rgx = calculate_Rg2x(d,tw,r,h,b,tf)  !Radius of Gyration X
         Rgy = calculate_Rg2y(d,tw,r,h,b,tf)  !Radius of Gyration Y
-        Rg = calculate_Rg2(d,tw,r,h,b,tf)  !Radius of Gyration
         SMx = calculate_SMx2(d,tw,r,h,b,tf)  !Section Modulus x
         SMz = calculate_SMz2(d,tw,r,h,b,tf)  !Section Modulus y
         Cx = calculate_Cx2(d,tw,r,h,b,tf)  !Centroid X
         Cy = calculate_Cy2(d,tw,r,h,b,tf)  !Centroid y
        Pmx = calculate_Pmx2(d,tw,r,h,b,tf)
+       elseif (input .eq. "COMPOUND" .or. input .eq. "compound") then
+       xcc = calculate_xc(b)
+       ycc = calculate_yc(A1,d,A2,cy)
+       ixx = calculate_Ix(Ixx1,A1,Ixx2,cy,d)
+       iyy = calculate_Iy(Iyy1,A1,Iyy2,A2)
+       
+       
         else
         write(*,*)"INVALID PROFILE, Press Enter to Start Again......"
         read(*,*)
         go to 100
         end if
 
+
         ! print the results
         write(*,40)
   40    format(/,"---------------Properties---------------")
+        if(profileType .eq. "H" .or. profileType .eq. "U") then
         write(*,22) X
   22    format(/,"Ixx =",20x,f15.2,1x," mm^4")
         write(*,23) Z
@@ -108,9 +134,9 @@
         write(*,25) Area
   25    format(/,"Area =",19x,f15.2,1x," mm^2")
         write(*,26) Rgx
-  26    format(/,"Radius of Gyration X =",5x, f15.2,1x," mm")
+  26    format(/,"Radius of Gyration X =",3x, f15.2,1x," mm")
         write(*,33) Rgy
-  33    format(/,"Radius of Gyration Y =",5x, f15.2,1x," mm")
+  33    format(/,"Radius of Gyration Y =",3x, f15.2,1x," mm")
         write(*,27) SMx
   27    format(/,"Section Modulus x =",6x,f15.2,1x," mm^3")
         write(*,28) SMz
@@ -119,8 +145,18 @@
   29    format(/,"Centroid X =",13x,f15.2,1x, " mm")
         write(*,30) Cy
   30    format(/,"Centroid Y =",13x,f15.2,1x, " mm")
-        write(*,31) Pmx
-  31    format(/,"Plastic Modulus =",8x,f15.2,1x, " mm^3")
+        write(*,94) Pmx
+  94    format(/,"Plastic Modulus =",8x,f15.2,1x, " mm^3")
+        else
+        write(*,95) xcc
+  95    format(/,"Centroid X =",12x,f15.2,2x, " mm")
+        write(*,96) ycc
+  96    format(/,"Centroid Y =",12x,f15.2,2x, " mm")
+        write(*,97) ixx
+  97    format(/,"Ixx =",18x,f15.2,2x, " mm^4")
+        write(*,98) iyy
+  98    format(/,"Iyy =",18x,f15.2,2x, " mm^4")
+        end if
 
   200   write(*,32)
   32    format(/,"Do you want to start again (Y/N):")
@@ -135,6 +171,7 @@
         end if
 
 
+
       contains
 
       !FUNCTIONS for I/H profiles
@@ -143,7 +180,7 @@
       Ax =(b * d**3) / 12 - ((b - tw) * h**3) / 12   !Ixx
       end function calculate_Ax
 
-      function calculate_Az(d,tw,r,h,b,tf) result(Az)                !Izz
+      function calculate_Az(d,tw,r,h,b,tf) result(Az)   !Izz
       real :: Az
       Az = (h*tw**3)/12 + 2*((tf*b**3)/12.0)
       end function calculate_Az
@@ -234,11 +271,33 @@
         Cy2 = d/2
         end function calculate_Cy2
         
-        
       function calculate_Pmx2(d,tw,r,h,b,tf) result(Pmx2)
       real :: Pmx2
       Pmx2 =(Area/2)*2*((b*tf)*0.5*(h+tf)+(tw*h**2)/8)/(b*tf + h*tw/2)
       end function calculate_Pmx2
+      
+      !! for composite shapes
+      function calculate_xc(b) result(xc)
+      real :: xc
+      xc = b/2
+      end function calculate_xc
+      
+      function calculate_yc(A1,d,A2,cy) result(yc)       !you need to fill out the J variable
+      real :: yc
+      yc = (A1*(d/2) + A2*(d+cy))/(A1+A2)
+      end function calculate_yc
+      
+      function calculate_Ix(Ixx1,A1,Ixx2,cy,d) result(Ix)   !you need to fill the d variables
+      real :: Ix
+      Ix = (Ixx1 + (A1*ABS(ycc-d/2)**2)) + (Ixx2 + (A2*ABS(ycc-cy)**2))
+      end function calculate_Ix
+      
+      function calculate_Iy(Iyy1,A1,Iyy2,A2) result(Iy)
+      real :: Iy
+      Iy = (Iyy1 + (A1)) + (Iyy2 + A2)
+      end function calculate_Iy
+      
+      
       
         subroutine get_measurement_from_the_user()
         write(*,*) "Please insert profile type(H or U): "
@@ -251,6 +310,8 @@
         read(*,*) tw
         write(*,*)"Please insert flange thickness tf in mm: "
         read(*,*) tf
+        write(*,*)"Please insert web radius r in mm: "
+        read(*,*) r
         write(*,*)"Please insert web height h in mm: "
         read(*,*) h
         end subroutine
